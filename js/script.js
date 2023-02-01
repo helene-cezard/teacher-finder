252068aace90d432f71d03f37ff84e6722bc4047
helene-cezard <helene.cezard@gmail.com>
2023-02-01 22:27:18 +0100

    Filtre par spécialité

--- js/script.js
+++ js/script.js
@@ -41,24 +41,33 @@ const app = {
   init: function() {
     console.log('Je suis dans la fonction init');
 
-    app.createSelect();
-    // app.createCounter();
-    // app.createList();
+    app.createLanguageSelect();
+    app.createSpecialitySelect();
 
     const languageSelect = document.querySelector('.languages');
     languageSelect.addEventListener('change', app.filterByLanguage);
+
+    const specialitySelect = document.querySelector('.specialities');
+    specialitySelect.addEventListener('change', app.filterBySpeciality);
   },
 
-  createSelect: function() {
-    console.log('Je suis dans la fonction createSelect');
+  createLanguageSelect: function() {
+    console.log('Je suis dans la fonction createLanguageSelect');
 
     const container = document.getElementById('app');
     const form = document.createElement('form');
+    form.setAttribute('class', 'selectForm');
     container.appendChild(form);
 
     const select = document.createElement('select');
     select.setAttribute('class', 'languages');
     form.appendChild(select);
+
+    const placeHolder = document.createElement('option');
+    placeHolder.textContent = 'Choisissez un language';
+    placeHolder.setAttribute('disabled', 'disabled');
+    placeHolder.setAttribute('selected', 'selected');
+    select.appendChild(placeHolder);
 
     const javaScriptOption = document.createElement('option');
     javaScriptOption.textContent = 'JavaScript';
@@ -71,16 +80,54 @@ const app = {
     select.appendChild(phpOption);
   },
 
-  createCounter: function(count) {
+  createSpecialitySelect: function() {
+    console.log('Je suis dans la fonction createSpecialitySelect');
+
+    const form = document.querySelector('.selectForm');
+
+    const select = document.createElement('select');
+    select.setAttribute('class', 'specialities');
+    form.appendChild(select);
+
+    const placeHolder = document.createElement('option');
+    placeHolder.textContent = 'Choisissez une spécialité';
+    placeHolder.setAttribute('disabled', 'disabled');
+    placeHolder.setAttribute('selected', 'selected');
+    select.appendChild(placeHolder);
+
+    const reactOption = document.createElement('option');
+    reactOption.textContent = 'React';
+    reactOption.setAttribute('value', 'React');
+    select.appendChild(reactOption);
+
+    const symfonyOption = document.createElement('option');
+    symfonyOption.textContent = 'Symfony';
+    symfonyOption.setAttribute('value', 'Symfony');
+    select.appendChild(symfonyOption);
+
+    const wordpressOption = document.createElement('option');
+    wordpressOption.textContent = 'WordPress';
+    wordpressOption.setAttribute('value', 'WordPress');
+    select.appendChild(wordpressOption);
+
+    const dataOption = document.createElement('option');
+    dataOption.textContent = 'Data';
+    dataOption.setAttribute('value', 'Data');
+    select.appendChild(dataOption);
+  },
+
+  createOrModifyCounter: function() {
+
     if (document.querySelector('.counter') === null) {
     
-      const select = document.querySelector('.languages');
+      const form = document.querySelector('.selectForm');
       const counter = document.createElement('div');
       counter.textContent = this.nbTeachers + ' profs trouvés';
       counter.setAttribute('class', 'counter');
-      select.after(counter);
+      form.after(counter);
 
     } else {
+
       const counter = document.querySelector('.counter');
       counter.textContent = this.nbTeachers + ' profs trouvés';
     }
@@ -109,6 +156,10 @@ const app = {
       const tag = document.createElement('span');
       tag.textContent = teacher.base;
       listElement.appendChild(tag);
+
+      const SpecialityTag = document.createElement('span');
+        SpecialityTag.textContent = teacher.speciality;
+        listElement.appendChild(SpecialityTag);
     }
   },
 
@@ -119,9 +170,20 @@ const app = {
 
     app.nbTeachers = languageTeachers.length;
 
-    app.createCounter();
+    app.createOrModifyCounter();
     app.createList(languageTeachers);
   },
+
+  filterBySpeciality: function(event) {
+    const specialityTeachers = app.teachers.filter(
+      (teacher) => event.target.value === teacher.speciality
+    );
+
+    app.nbTeachers = specialityTeachers.length;
+
+    app.createOrModifyCounter();
+    app.createList(specialityTeachers);
+    },
 
 };
 
