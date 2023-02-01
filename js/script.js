bbadc33f17e04a2df9b4649d7c88ad8f9ca020cf
helene-cezard <helene.cezard@gmail.com>
2023-02-01 16:58:18 +0100

    Création du sélecteur de spécialité

--- js/script.js
+++ js/script.js
@@ -2,6 +2,30 @@ const app = {
 
   init: function() {
     console.log('Je suis dans la fonction init');
+
+    app.createSelect();
+  },
+
+  createSelect: function() {
+    console.log('Je suis dans la fonction createSelect');
+
+    const container = document.getElementById('app');
+    const form = document.createElement('form');
+    container.appendChild(form);
+
+    const select = document.createElement('select');
+    select.setAttribute('class', 'languages');
+    form.appendChild(select);
+
+    const javaScriptOption = document.createElement('option');
+    javaScriptOption.textContent = 'JavaScript';
+    javaScriptOption.setAttribute('value', 'JavaScript');
+    select.appendChild(javaScriptOption);
+
+    const phpOption = document.createElement('option');
+    phpOption.textContent = 'PHP';
+    phpOption.setAttribute('value', 'PHP');
+    select.appendChild(phpOption);
   },
 
 };
